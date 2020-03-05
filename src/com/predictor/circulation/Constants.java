a5e4a9c2cfcf5eff3d433a5dc0b530d324e402f1
Alejandro B <alexander.br6@gmail.com>
2020-03-05 00:21:12 -0500

    Add constants in class Constants to centralize this ones
    Add methods to validate plate, date and hour in main class

--- src/com/predictor/circulation/Constants.java
+++ src/com/predictor/circulation/Constants.java
@@ -1,9 +1,18 @@
 package com.predictor.circulation;
 
 public final class Constants {
-	public static final String DATE_REGEX = ("^((2000|2400|2800|(19|2[0-9](0[48]|[2468][048]|[13579][26])))-02-29)$"
+	
+	// Class to declare constants and centralize this ones
+	// Date pattern
+	public static final String DATE_PATTERN = ("^((2000|2400|2800|(19|2[0-9](0[48]|[2468][048]|[13579][26])))-02-29)$"
 		      + "|^(((19|2[0-9])[0-9]{2})-02-(0[1-9]|1[0-9]|2[0-8]))$"
 		      + "|^(((19|2[0-9])[0-9]{2})-(0[13578]|10|12)-(0[1-9]|[12][0-9]|3[01]))$"
 		      + "|^(((19|2[0-9])[0-9]{2})-(0[469]|11)-(0[1-9]|[12][0-9]|30))$");
 	
+	// Hour pattern
+	public static final String HOUR_PATTERN = ("([01]?[0-9]|2[0-3]):[0-5][0-9]");
+	
+	// Max & min length of number of plate
+	public static final int MIN_NUMBER_PLATE = 3;
+	public static final int MAX_NUMBER_PLATE = 4;
 }
--- src/com/predictor/circulation/Main.java
+++ src/com/predictor/circulation/Main.java
@@ -1,13 +1,107 @@
 package com.predictor.circulation;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
 	static Service service = new Service();
+	static Scanner sc = new Scanner(System.in);
 	
 	public Main() {
 		
 	}
+	
+	// Method to validate the length of number of plate
+	public static int plateOfConsultant(String message) {
+		int intPlate = 0;
+		boolean verificationPlateNumber = true;
 
+		while (verificationPlateNumber == true) {
+			System.out.println(message);
+			String stringPlate = sc.nextLine();
+			if (stringPlate.length() == Constants.MIN_NUMBER_PLATE | stringPlate.length() == Constants.MAX_NUMBER_PLATE) {
+				try {
+					intPlate = Integer.parseInt(stringPlate);
+					verificationPlateNumber = false;
+				} catch (Exception e) {
+					System.err.println("The plate must be a number ... ");
+				}
+			} else {
+				System.err.println("The plate must be have 3 or 4 numbers ... ");
+			}
+
+		}
+		return intPlate;
+	}
+	
+	// Method to validate the correct pattern of date
+	public static String dateOfConsult(String message) {
+		boolean verificationDate = true;
+		String dateEntered = "";
+
+		while (verificationDate == true) {
+			System.out.println(message);
+			dateEntered = sc.nextLine();
+			Pattern pattern = Pattern.compile(Constants.DATE_PATTERN);
+			Matcher matcher = pattern.matcher(dateEntered);
+			
+			if (matcher.matches()) {
+				try {
+					verificationDate = false;
+				} catch (Exception e) {
+					System.err.println("Enter the date with the correct format ... ");
+				}
+			} else {
+				System.err.println("The format is not correct ... ");
+			}
+		}
+		return dateEntered;
+	}
+	
+	// Method to validate the correct pattern of hour entered
+	public static String hourOfConsult(String message) {
+		boolean verificationDate = true;
+		String hourEntered = "";
+
+		while (verificationDate == true) {
+			System.out.println(message);
+			hourEntered = sc.nextLine();
+			Pattern pattern = Pattern.compile(Constants.HOUR_PATTERN);
+			Matcher matcher = pattern.matcher(hourEntered);
+			
+			if (matcher.matches()) {
+				try {
+					verificationDate = false;
+				} catch (Exception e) {
+					System.err.println("Enter the hour with the correct format ... ");
+				}
+			} else {
+				System.err.println("The format is not correct ... ");
+			}
+		}
+		return hourEntered;
+	}
+	
+	// Methods to recive the message and data of each one
+	public static void enterPlate() {
+		int dataPlate = plateOfConsultant("Enter a plate number");
+		service.dinamicInsertPlateNumber(new PlateNumber(dataPlate));
+	}
+
+	public static void enterDate() {
+		String dataDate = dateOfConsult("Enter the date: year-month-day");
+		service.dinamicInsertDateEntered(new DateEntered(dataDate));
+	}
+
+	public static void enterHour() {
+		String dataHour = hourOfConsult("Enter the hour: hour:minute 24hr");
+		service.dinamicInsertHourEntered(new HourEntered(dataHour));
+	}
+	
 	public static void main(String[] args) {
-
+		enterPlate();
+		enterDate();
+		enterHour();
 	}
 }
